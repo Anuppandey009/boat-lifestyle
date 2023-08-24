84d90dd4829787b7949bf5d71a89c66da9f45868
kanika INT <malikkanika9@gmail.com>
2023-08-24 12:44:08 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/Navbar/NavBar';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
-     
+      <NavBar />
+      <ScrollToTop/>
     </div>
   );
 }
---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.css"
+export const NavBar = () => {
+    return (
+
+        <>
+            <div className="navbar">
+                <div className="pre-navbar">
+                    <p>Get legendary sound at a legendary price. <b>Airpods 131 </b> is now at <b>₹999</b>. Shop Now! </p>
+                </div>
+
+            </div>
+        </>
+    )
+
+}
