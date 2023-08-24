a779afa45930d2e4fc965d063d9d8235474331bd
Anup Pandey <anup.k.pandey@indusnet.co.in>
2023-08-24 12:42:09 +0530

    Home Carousel added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeCarousel from "./components/HomeCarousel/HomeCarousel";
+
+
+
 
 function App() {
   return (
     <div className="App">
-     
+    <HomeCarousel/>
     </div>
   );
 }
