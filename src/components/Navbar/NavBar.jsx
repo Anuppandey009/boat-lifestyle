82120312f2dd33a4375e4e89c155fc957009ed51
kanika INT <malikkanika9@gmail.com>
2023-08-25 12:43:00 +0530

    done

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -16,12 +16,12 @@ export const NavBar = () => {
   return (
     <>
       <div className="navbar">
-        {/* <div className="pre-navbar">
+        <div className="pre-navbar">
           <p>
             Get legendary sound at a legendary price. <b>Airpods 131 </b> is now
             at <b>₹999</b>. Shop Now!{" "}
           </p>
-        </div> */}
+        </div>
       </div>
 
       <div className="parent-container">
